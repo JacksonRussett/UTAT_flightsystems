d68b608b02b45d93ea33a46d17df1dcfcf6aad48
Vedang Naik <vedang.a.naik@gmail.com>
2019-11-08 12:20:30 -0400

    Vedang's first commit

---
+++ Code/Flight Code/sensorIntegration/main/Debug/Adafruit_INA219/Adafruit_INA219.cpp.d
@@ -0,0 +1,6 @@
+C:\Users\vedan\AppData\Local\Temp\VMBuilds\main\teensy31\Debug\Adafruit_INA219\Adafruit_INA219.cpp.o: \
+ C:\Users\vedan\Documents\Arduino\libraries\Adafruit_INA219\Adafruit_INA219.cpp \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\avr\libraries\Wire/Wire.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\avr\libraries\Wire/WireKinetis.h \
+ C:\Users\vedan\AppData\Local\Temp\VMBuilds\main\teensy31\Debug/pch/Arduino.h \
+ C:\Users\vedan\Documents\Arduino\libraries\Adafruit_INA219\Adafruit_INA219.h
---
+++ Code/Flight Code/sensorIntegration/main/Debug/Wire/utility/twi.c.d
@@ -0,0 +1,2 @@
+C:\Users\vedan\AppData\Local\Temp\VMBuilds\main\teensy31\Debug\Wire\utility\twi.c.o: \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\avr\libraries\Wire\utility\twi.c
